25b6bb7d39abe4949ca2ee4c2639d571eb1f8eb6
helpfulrobot <chris@silverstripe.com>
2015-12-18 10:39:28 +1300

    Converted to PSR-2

--- code/Embed/Embed.php
+++ code/Embed/Embed.php
@@ -1,53 +1,64 @@
 <?php
 
-abstract class FileEmbed extends ViewableData {
-	
-	/**
-	 * Generates the necessary XML namespace identifiers required for this document
-	 * 
-	 * @return string
-	 */
-	public function getXMLNS() {}
-	
-	/**
-	 * Generates any necessary head tags for this document
-	 * 
-	 * @return string
-	 */
-	public function getHead() {}
-	
-	/**
-	 * Gets the width of a file
-	 * 
-	 * @param string $file Path to file
-	 * @return integer
-	 */
-	public function getWidth($file) {
-		$imagefile = realpath(BASE_PATH . '/' . $file);
-		if(empty($imagefile)) return null;
-		$size = getimagesize($imagefile);
-		return $size[0];
-	}
-	
-	/**
-	 * Gets the height of a file
-	 * 
-	 * @param string $file Path to file
-	 * @return integer
-	 */
-	public function getHeight($file) {
-		$imagefile = realpath(BASE_PATH . '/' . $file);
-		if(empty($imagefile)) return null;
-		$size = getimagesize($imagefile);
-		return $size[1];
-	}
-	
-	/**
-	 * Embed a file
-	 * 
-	 * @param string $url URL or path to file
-	 * @param string $alt alt text for this file
-	 * @return string HTML for this image
-	 */
-	abstract function Image($url, $alt = '');
+abstract class FileEmbed extends ViewableData
+{
+    
+    /**
+     * Generates the necessary XML namespace identifiers required for this document
+     * 
+     * @return string
+     */
+    public function getXMLNS()
+    {
+    }
+    
+    /**
+     * Generates any necessary head tags for this document
+     * 
+     * @return string
+     */
+    public function getHead()
+    {
+    }
+    
+    /**
+     * Gets the width of a file
+     * 
+     * @param string $file Path to file
+     * @return integer
+     */
+    public function getWidth($file)
+    {
+        $imagefile = realpath(BASE_PATH . '/' . $file);
+        if (empty($imagefile)) {
+            return null;
+        }
+        $size = getimagesize($imagefile);
+        return $size[0];
+    }
+    
+    /**
+     * Gets the height of a file
+     * 
+     * @param string $file Path to file
+     * @return integer
+     */
+    public function getHeight($file)
+    {
+        $imagefile = realpath(BASE_PATH . '/' . $file);
+        if (empty($imagefile)) {
+            return null;
+        }
+        $size = getimagesize($imagefile);
+        return $size[1];
+    }
+    
+    /**
+     * Embed a file
+     * 
+     * @param string $url URL or path to file
+     * @param string $alt alt text for this file
+     * @return string HTML for this image
+     */
+    abstract public function Image($url, $alt = '');
 }
--- code/Embed/LinkEmbed.php
+++ code/Embed/LinkEmbed.php
@@ -1,19 +1,21 @@
 <?php
 
-class LinkEmbed extends FileEmbed {
-	
-	public function Image($url, $alt = '') {
-		$altattr = Convert::raw2att($alt);
-		$imageSRC = Convert::raw2att(Director::absoluteURL($url));
-		$width = $this->getWidth($url);
-		$height = $this->getHeight($url);
-		return
-			"<img
+class LinkEmbed extends FileEmbed
+{
+    
+    public function Image($url, $alt = '')
+    {
+        $altattr = Convert::raw2att($alt);
+        $imageSRC = Convert::raw2att(Director::absoluteURL($url));
+        $width = $this->getWidth($url);
+        $height = $this->getHeight($url);
+        return
+            "<img
 				border=\"0\"
 				src=\"$imageSRC\"
 				width=\"$width\"
 				height=\"$height\"
 				alt=\"$altattr\"
 			/>";
-	}
+    }
 }
